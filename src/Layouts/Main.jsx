71077367df778ebdc580e529eea78161ccf5a3b8
Md. Abir Mahmud <abirmahmud5665@gmail.com>
2023-07-03 15:28:27 +0600

    implemented loading anim

--- src/Layouts/Main.jsx
+++ src/Layouts/Main.jsx
@@ -1,7 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Header from "../Pages/Shared/Header/Header";
+import useExProvider from "../hooks/useExProvider";
+import Loading from "../Pages/Loading/Loading";
 
 const Main = () => {
+  const { loading } = useExProvider();
+  if (loading) {
+    return <Loading />;
+  }
   return (
     <>
       <Header />
---
+++ src/Pages/Loading/Loading.jsx
@@ -0,0 +1,17 @@
+import Lottie from "lottie-react";
+import loadingAnim from "../../assets/loading/loading-anim-2.json";
+import logo from "/shop-ex-logo.svg";
+const Loading = () => {
+  return (
+    <div className="w-full h-screen flex justify-center items-center flex-col">
+      <img src={logo} className="w-20 md:w-40" alt="Logo" />
+      <Lottie
+        className="w-10 h-10 md:w-20 md:h-20"
+        animationData={loadingAnim}
+        loop={true}
+      />
+    </div>
+  );
+};
+
+export default Loading;
